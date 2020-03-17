2cadd66e056b8fb96184fb38210c19cdc3e4a301
anilchoudhary <masteranilchoudhary@gmail.com>
2020-03-17 10:28:45 +0530

    done excercises of chapter 7

--- rango_project/urls.py
+++ rango_project/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('rango/', include('rango.urls')),
+    path('rango/', include('rango.urls', namespace="rango")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
